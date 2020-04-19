4c70fb591d77e212f153ddba42670acf5068a0af
annaglass <63213423+annaglass@users.noreply.github.com>
2020-04-19 17:10:06 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,10 @@
-/* OUTLINE
-Program starts by setting the scene and describing the world the adventure 
-game is set in, then you are taken to the first challenge: choose your animal. 
-You choose which animal you want to tame, give it a name, and provide your 
-username. A text file will be read in, with all the animals and their levels 
-for different skills, and you will pick between 3 animals for each tier. For 
-the first tier or first animal you tame, the options are a ferret, parrot, or 
-monkey, the second tier is a wolf, snow leopard, or owl, and finally when you 
-have trained your animals from the two tiers to a sufficient level, you will 
-unlock the polar bear: once you have tamed it you have won the game. 
-The user is taken through a series of challenges in different locations, 
-their animal increases their level in a given skill if the user makes the right 
-decisions in the challenge, else they will loop back to the beginning of the 
-challenge and must try again. When you have gone through level one of each of 
-the challenges, the program will display the animal’s stats and give you a menu
-for different challenges you can choose to do to further improve the animal’s 
-skills to the necessary level to move on. Each animal will need to go through 
-a certain number of levels to be trained in that skill, Once you have finished 
-enough levels to tame the animal, you will have the option to either quit the 
-game, pick an animal from the next tier, or continue moving through the levels 
-of the challenges. After finishing the second tier you unlock the polar bear, 
-where instead of having challenges related to improving your skill, you look 
-for the golden compass with the help of the polar bear to eventually win the 
-game. Along the way you can collect gold to buy certain items, and random 
-challenges will pop up that can either give bonus points to your animal’s 
-skill, or will cause your animal to run away which will cause you to go back 
-to the beginning. */
 
-// need array of objects
-// http://www.cplusplus.com/reference/cstdlib/rand/
+int main() {
+  play.readAnimals(filename);
+  menus.welcome();
+  play.playChallenges();
+  
+  
+  
+  
+}
