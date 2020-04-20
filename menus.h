3879c24327f083703e58ebeb78d549b668d294e7
annaglass <63213423+annaglass@users.noreply.github.com>
2020-04-19 22:01:28 -0600

    Update menus.h

--- menus.h
+++ menus.h
@@ -1,18 +1,10 @@
-Class Menus{
+Class Menus {
 	Private:
 		int userInput;
 	Public:
-		string animalMenuTier1( ); //3 animal options for tier 1, displays stats
-			// cout << “enter a number” << endl
-			
-		string animalMenuTier2( ); //3 animal options for tier 2, displays stats
-		int challengeMenu1( ); //displays levels (including what is completed and what isn’t), returns the level user chooses
-		int challengeMenu2( );
-			// cout << enter a level << endl;
-			// cin >> level
-		int challengeMenu3( );
-		int challengeMenu4( );
-		int challengeMenu5( );
-		int challengeMenu6( );
-		int endOfTierMenu( );
+		Menus();
+		void welcome();
+		string animalMenu();	
+		int endOfTierMenu();
+		int endOfChallenge();
 };
