c914f793f9542ef6810095e5a6f39838a6650ef1
annaglass <63213423+annaglass@users.noreply.github.com>
2020-04-19 22:47:20 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,7 @@
-
 int main() {
   play.readAnimals(filename);
   menus.welcome();
   play.playChallenges();
-  
-  
-  
-  
+  //if all the scores are 5 or more, allow user to play elephant challenge
+  play.ElephantChallenge();
 }
