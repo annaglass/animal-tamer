e391cc6dc079cc68dc4ffa6f65a3d9915e34e885
annaglass <63213423+annaglass@users.noreply.github.com>
2020-04-19 22:07:52 -0600

    Update play.h

--- play.h
+++ play.h
@@ -8,19 +8,9 @@ using namespace std;
 
 Class Play{
 	Private:
-		Int userChoice;
 		Animals animals[7];
 	Public:
+		Play();
 		void readAnimals(string);
-		void playChallenge1(int);
-			// call menu for the levels
-			// cout << enter a level << endl;
-			// cin >> level
-			// user that cin value for the switch statement
-			// change challenges matrix based on if they finished the level or not
-		void playChallenge2( );
-		void playChallenge3( );
-		void playChallenge4( );
-		void playChallenge5( );
-		void playChallenge6( );
+		void playChallenges(int);
 };
